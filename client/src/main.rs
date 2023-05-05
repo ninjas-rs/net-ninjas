568749474624e913c71392d84cdb82b33c927fce
Syed Ahkam <smahkam57@gmail.com>
2023-05-05 14:18:18 +0530

    Add wasm support with trunk ✨

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PresentMode};
 
 #[cfg(feature = "editor")]
 use bevy_editor_pls::EditorPlugin;
@@ -8,13 +8,23 @@ mod assets;
 mod navigation;
 mod ui;
 
+const APP_TITLE: &str = "Net Ninjas 🥷";
+
 fn main() {
     let mut app = App::new();
 
-    app.add_plugins(DefaultPlugins)
-        .add_plugin(navigation::NavigationPlugin)
-        .add_plugin(ui::UiPlugin)
-        .add_startup_system(setup);
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: APP_TITLE.to_string(),
+            present_mode: PresentMode::AutoNoVsync, // Reduces input lag.
+            fit_canvas_to_parent: true,
+            ..default()
+        }),
+        ..default()
+    }))
+    .add_plugin(navigation::NavigationPlugin)
+    .add_plugin(ui::UiPlugin)
+    .add_startup_system(setup);
 
     #[cfg(feature = "editor")]
     app.add_plugin(EditorPlugin::default()); // only available if the "editor" feature is enabled
@@ -23,5 +33,5 @@ fn main() {
 }
 
 fn setup() {
-    println!("Welcome to Net Ninjas 🥷")
+    println!("Welcome to {}!", APP_TITLE);
 }
